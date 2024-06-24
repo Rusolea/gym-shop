83d2774c578ce92734da3632aa536aa13379f543
Osvaldo Juan Olea <ruso@MacBook-Air-de-Osvaldo.local>
2024-06-24 16:14:35 -0300

    agrego about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,17 @@
-// import { Routes } from '@angular/router';
-// import { ProductListComponent } from './product-list/product-list.component';
-// import { CartComponent } from './cart/cart.component';
-
-// export const routes: Routes = [
-//   { path: '', component: ProductListComponent },
-//   { path: 'cart', component: CartComponent }
-// ];
-
-
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  { path: 'about', component: AboutComponent}
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
